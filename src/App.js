0d0883f7a6322a13d790736d9629d473b7d9ae23
Keshav Narula <narulakeshav13@gmail.com>
2017-03-09 23:53:25 -0800

    Format

--- src/App.js
+++ src/App.js
@@ -1,72 +1,66 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import InputItem from './components/InputItem';
+import TaskBlock from './components/TaskBlock';
 
 // Import CSS
 import '../public/css/design.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todayTasks: {
-        // From 12-3
-        twelveToThree: {
-          twelve: { items: [] },
-          one: { items: [] },
-          two: { items: [] },
-          three: { items: [] }
-        },
-        // From 4-7
-        fourToSeven: {
-          four: { items: [] },
-          five: { items: [] },
-          six: { items: [] },
-          seven: { items: [] }
-        },
-        // From 8-11
-        eightToEleven: {
-          eight: { items: [] },
-          nine: { items: [] },
-          ten: { items: [] },
-          eleven: { items: [] }
-        }
-      }
-    };
-    this._addTask = this._addTask.bind(this);
-  }
-  render() {
-    return (
-      <div className="App">
-        <InputItem addTask={this._addTask}/>
-      </div>
-    );
-  }
-
-  /**
-   * Adds task to the todayTasks state
-   * @param {object} task
-   * task: { 
-   *    timeRange: 'twelveToThree' 
-   *    startTime: 'three',
-   *    endTime: 'four',
-   *    do: ['Sleep', 'Eat']
-   * }
-   */
-  _addTask(task) {
-    let tasks = this.state.todayTasks;
-    
-    // Pushes and add and array to the end of 'taskArr'
-    let taskArr = tasks[task.timeRange][task.startTime].items;
-
-    // Push items to the array
-    for (let item of task.do) {
-      taskArr.push(item);
+    constructor() {
+        super();
+        this.state = {
+            todayTasks: {
+                // From 12-3
+                twelveToThree: {
+                    range: '12–3 PM',
+                    items: []
+                },
+                // From 4-7
+                fourToSeven: {
+                    range: '4–7 PM',
+                    items: []
+                },
+                // From 8-11
+                eightToEleven: {
+                    range: '8–11 PM',
+                    items: []
+                }
+            }
+        };
+        this._addTask = this._addTask.bind(this);
+    }
+    render() {
+        return (
+            <div className="App">
+                <InputItem addTask={this._addTask}/>
+                <TaskBlock tasks={this.state.todayTasks}/>
+            </div>
+        );
     }
 
-    // Updates the state
-    this.setState({ todayTasks: tasks });
-    console.log(this.state.todayTasks);
-  }
+    /**
+   * Adds task to the todayTasks state
+   * @param {object} task
+   * [ { time: '3-4', items: [] } ]
+   */
+    _addTask(task) {
+        let tasks = this.state.todayTasks;
+
+        // Pushes and add and array to the end of 'taskArr'
+        let taskArr = tasks[task[0].timeRange].items;
+
+        // Push items to the array
+        for (let item of task) {
+            taskArr.push({
+                time: item.time,
+                do : item.do
+                    }
+                );
+        }
+
+        // Updates the state
+        this.setState({todayTasks: tasks});
+    }
 }
 
 export default App;
